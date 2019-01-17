d57725252c4765c39a3d2bcee8c4070e4cf5d193
Matthieu Coudron <mattator@gmail.com>
2019-01-17 23:50:06 +0900

    git: add support for send-email
    now works with gmail
    
    trying to take into account mbox
    
    Adding coontact completion to alot
    
    missing pkgs
    
    removed the pkgs thing
    
    correctly escapes the regex
    
    adding abook type
    
    merged

--- modules/programs/neomutt-accounts.nix
+++ modules/programs/neomutt-accounts.nix
@@ -6,27 +6,14 @@ with lib;
   options.neomutt = {
     enable = mkEnableOption "NeoMutt";
 
-      # TODO we need some logic on how to 
-      # externalMta = mkOption {
-      #   type = types.bool;
-      #   default = true;
-      #   description = "Use external smtp";
-      # };
-
-      externalMra = mkOption {
-        type = types.bool;
-        default = true;
-        description = "Use external fetcher";
-      };
-
-    # set new_mail_command = ""
-    onNewMailCommand = mkOption {
-      type = types.nullOr types.str;
-      description = ''
-        <command>msmtpq --read-envelope-from --read-recipients</command>.
-      '';
+    # TODO we need some logic on how to 
+    externalMta = mkOption {
+      type = types.bool;
+      default = true;
+      description = "Use external smtp";
     };
 
+<<<<<<< HEAD
     sendMailCommand = mkOption {
       type = types.nullOr types.str;
       default = if config.msmtp.enable then
@@ -44,21 +31,82 @@ with lib;
         Command to send a mail. If not set, neomutt will be in charge of sending mails.
       '';
     };
-
-    imap.idle = mkOption {
-      type = types.bool;
-      default = false;
+||||||| parent of d7879f6a... now works with gmail
+    sendMailCommand = mkOption {
+      type = types.nullOr types.str;
       description = ''
-        If set, neomutt will attempt to use the IDLE extension.
+        Command to send a mail. If msmtp is enabled for the account,
+        then this is set to
+        <command>msmtpq --read-envelope-from --read-recipients</command>.
+      '';
+    };
+=======
+    # externalMta = mkOption {
+    #   type = types.bool;
+    #   default = true;
+    #   description = "Use external MTA.";
+    # };
+>>>>>>> d7879f6a... now works with gmail
+
+    # externalMra = mkOption {
+    #   type = types.bool;
+    #   default = true;
+    #   description = "Use external MRA.";
+    # };
+    # internalImapModule = types.submodule {
+    #   options = {
+    #     idle = mkOption {
+    #       type = types.bool;
+    #       default = false;
+    #       description = ''
+    #         If set, neomutt will attempt to use the IDLE extension.
+    #       '';
+    #     };
+    #     host = mkOption {
+    #       type = types.str;
+    #       example = "imap.example.org";
+    #       description = ''
+    #         Hostname of IMAP server.
+    #       '';
+    #     };
+
+    #     port = mkOption {
+    #       type = types.nullOr types.ints.positive;
+    #       default = null;
+    #       example = 993;
+    #       description = ''
+    #         The port on which the IMAP server listens. If
+    #         <literal>null</literal> then the default port is used.
+    #       '';
+    #     };
+
+    #     tls = mkOption {
+    #       type = tlsModule;
+    #       default = {};
+    #       description = ''
+    #         Configuration for secure connections.
+    #       '';
+    #     };
+    #   };
+    # };
+
+    # set query_command="notmuch-addrlookup --mutt '%s'"
+    queryCommand = mkOption {
+      type = types.nullOr types.str;
+      # ${pkgs.notmuch-addrlookup}/bin/
+      default = "notmuch-addrlookup --mutt '%s'";
+      example = "notmuch-addrlookup --mutt '%s'";
+      description = ''
+        Command to send a mail. If not set, mutt will be in charge of sending mails.
       '';
     };
 
-    mailboxes = mkOption {
-      type = types.listOf types.str;
-      default = [];
-      example = ["github" "Lists/nix" "Lists/haskell-cafe"];
+    sendMailCommand = mkOption {
+      type = types.nullOr types.str;
+      default = null;
+      example = "msmtpq --read-envelope-from --read-recipients";
       description = ''
-        A list of mailboxes.
+        Command to send a mail. If not set, mutt will be in charge of sending mails.
       '';
     };
 
