ec09df5a19bd2fdd6a2205dc06dcf83defedac88
teto <886074+teto@users.noreply.github.com>
2026-02-16 04:35:03 +0100

    anki + mpv hacks

--- modules/programs/neovim.nix
+++ modules/programs/neovim.nix
@@ -476,7 +476,11 @@ in
       };
 
       home = {
-        packages = [ cfg.finalPackage ];
+        packages = [
+          # HACK until I get all the rest fixed
+          cfg.package
+          # cfg.finalPackage
+        ];
 
         sessionVariables = mkIf cfg.defaultEditor {
           EDITOR = "nvim";
